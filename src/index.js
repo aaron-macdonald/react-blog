af89f2c8129bf6dc569306e9f171bcf137578d03
Aaron Macdonald <aaronmac989@gmail.com>
2019-05-19 00:02:04 +1200

    re-001 Added a layout jsx and refactored ReactDOM

--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
-import FormContainer from "./js/components/container/FormContainer.jsx";
+import React from 'react'
+import ReactDOM from 'react-dom'
 
+import Layout from "./js/components/Layout.jsx"
+
+const placeToMount = document.getElementById('root')
+
+ReactDOM.render(
+  <Layout />,
+  placeToMount
+)
---
+++ src/js/components/Layout.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from "react-dom";
+
+function Layout() {
+  return (
+    <div className="layout">
+      <h1>Hello World</h1>
+    </div>
+  )
+}
+
+export default Layout
