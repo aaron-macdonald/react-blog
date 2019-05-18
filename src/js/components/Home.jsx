b081f20d8d9326aefab9ce40a1db2ab9f17658fb
Aaron Macdonald <aaronmac989@gmail.com>
2019-05-19 10:56:47 +1200

    re-001 public folder working and css

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -7,7 +7,7 @@ function Home (props) {
         <h4 className="center"></h4>
       </div>
       <div className="center">
-        <img src="public/images/game_002.jpg" alt="table with playing cards on it" height="700" width="1000"></img>
+        <img src="src/public/images/game_002.jpg" alt="table with playing cards on it" height="700" width="1000"></img>
       </div>
     </div>
   )
