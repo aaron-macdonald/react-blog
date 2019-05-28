c559ea969ce9a8c21ce762f781a650256295ebd0
Aaron Macdonald <aaronmac989@gmail.com>
2019-05-29 00:41:29 +1200

    zpf-001 Removed backend

--- server/index.js
+++
@@ -1,8 +0,0 @@
-/*eslint no-console: "off"*/
-const server = require('./server')
-
-const PORT = process.env.PORT || 5000
-
-server.listen(PORT, function () {
-  console.log('Listening on port', PORT)
-})
